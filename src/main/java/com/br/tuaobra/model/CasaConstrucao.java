58ff1092be01ddb9369b5ff66ff284173c76322f
Rian-Noronha <riannoronha011@gmail.com>
2024-09-20 12:00:54 -0300

    [feat] Criando campo urlListaOrcamento em CasaConstrucao

--- src/main/java/com/br/tuaobra/model/CasaConstrucao.java
+++ src/main/java/com/br/tuaobra/model/CasaConstrucao.java
@@ -28,6 +28,7 @@ public class CasaConstrucao {
 	private String descricao;
 	private String horario;
 	private String urlImagemPerfil;
+	private String urlListaOrcamento;
 	private String frete;
 	private String email;
 	private String contatoWhatsApp;

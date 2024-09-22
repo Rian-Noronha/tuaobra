250a5a05aaa5523a6ab3d0b49a2f753aacc6cd6f
Rian-Noronha <riannoronha011@gmail.com>
2024-09-22 10:34:04 -0300

    [feat] Criação PedreiroDemandaRepository

---
+++ src/main/java/com/br/tuaobra/repository/PedreiroDemandaRepository.java
@@ -0,0 +1,7 @@
+package com.br.tuaobra.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedreiroDemandaRepository extends JpaRepository<PedreiroDemandaRepository, Long>{
+
+}

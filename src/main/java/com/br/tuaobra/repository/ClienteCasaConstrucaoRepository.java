c1bf0fcd28193d71df146a11be0b488174af3426
Rian-Noronha <riannoronha011@gmail.com>
2024-09-22 10:37:32 -0300

    [feat] Criação da ClienteCasaConstrucaoRepository

---
+++ src/main/java/com/br/tuaobra/repository/ClienteCasaConstrucaoRepository.java
@@ -0,0 +1,9 @@
+package com.br.tuaobra.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.tuaobra.model.ClienteCasaConstrucao;
+
+public interface ClienteCasaConstrucaoRepository extends JpaRepository<ClienteCasaConstrucao, Long>{
+
+}

fb35d62d568df1be1c624fa579e282d3b6cf4c92
Paulo Henrique <148291338+PauloHenriqque@users.noreply.github.com>
2024-11-20 16:03:52 -0300

    refactor: modificando Pedreiro

--- src/main/java/com/br/tuaobra/model/Pedreiro.java
+++ src/main/java/com/br/tuaobra/model/Pedreiro.java
@@ -1,7 +1,12 @@
 package com.br.tuaobra.model;
 
+import java.util.Collection;
 import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -15,11 +20,12 @@ import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pedreiro {
+public class Pedreiro implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,6 +35,8 @@ public class Pedreiro {
 	private String nome;
 	private String contatoWhatsApp;
 	private String email;
+	@JsonIgnore
+	private String senha; // Campo necessário para autenticação
 
 	@ManyToMany
 	@JoinTable(name = "pedreiro_especialidade", joinColumns = @JoinColumn(name = "pedreiro_id"), inverseJoinColumns = @JoinColumn(name = "especialidade_id"))
@@ -46,4 +54,22 @@ public class Pedreiro {
 	@JsonIgnore
 	private List<Demanda> demandas;
 
+	@Override
+	@JsonIgnore
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// Aqui você pode adicionar diferentes roles se necessário. Exemplo:
+		return List.of(new SimpleGrantedAuthority("ROLE_PEDREIRO"));
+	}
+
+	@Override
+	@JsonIgnore
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	@JsonIgnore
+	public String getUsername() {
+		return this.email;
+	}
 }
